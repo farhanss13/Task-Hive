fcef9a3fd05485da1e0a3baafb8a740ce87e34ba
Farhan Saqib <farhansaqib67@gmail.com>
2026-04-07 02:15:47 +0500

    Added some logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,33 @@ import './App.css'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
-import { useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { setLocalStorage } from './utilities/LocalStorage'
+import { AuthContext } from './context/AuthProvider'
 
 function App() {
 
   useEffect(()=>{
     setLocalStorage()
   },)
+  const [user,setUser]=useState(null)
+  const authData = useContext(AuthContext)
 
+  const handleLogin = (email,password)=>{
+    if(email =="admin@me.com" && password =="123"){
+      setUser('admin')
+    } else if(authData && authData.employees.find((e)=>email == e.email && e.password == password)){
+      setUser('employee')
+    } else{
+      alert("Invalid Credentials!")
+    }
+  }
   return (
     <>
-    <Login/>
-    {/* <EmployeeDashboard/> */}
-    {/* <AdminDashboard/> */}
+    { !user?<Login handleLogin={handleLogin} />:" "}
+    {user=='admin'? <AdminDashboard/> :<EmployeeDashboard/>}
   
-  {/* start from 1:30:14 */}
-
+    {/* continue from 1:59:02 and add the useeffect hook */}
     </>
   )
 }
